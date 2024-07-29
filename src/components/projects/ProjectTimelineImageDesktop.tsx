9c316b103c0886d323bbf5604bc0c04343af4cab
Pawin Nakvisai <pawinnak2@gmail.com>
2024-07-29 15:24:56 +0700

    [ADD] Hide image when not loaded, project timeline

--- src/components/projects/ProjectTimelineImageDesktop.tsx
+++ src/components/projects/ProjectTimelineImageDesktop.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import { ProjectTimelineImageDesktopProps } from "../../types";
 
 function ProjectTimelineImageDesktop(props: ProjectTimelineImageDesktopProps) {
+    const [loaded, setIsLoaded] = useState(false);
+    const imageStyle = !loaded ? { display: "none" } : {}
+
     return (
-        <img
-            src={props.img[props.startIndex + props.colIndex].url}
-            alt={props.img[props.startIndex + props.colIndex].alt}
-            className={`project-img-${props.count} rounded-3 m-3`}
-        />
+        <>
+            <img
+                src={props.img[props.startIndex + props.colIndex].url}
+                alt={props.img[props.startIndex + props.colIndex].alt}
+                className={`project-img-${props.count} rounded-3 m-3`}
+                onLoad={() => setIsLoaded(true)}
+                style={imageStyle}
+            />
+        </>
     );
 }
 
--- src/components/projects/ProjectTimelineImageMobile.tsx
+++ src/components/projects/ProjectTimelineImageMobile.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import { ProjectTimelineImageMobileProps } from "../../types";
 
 function ProjectTimelineImageMobile(props: ProjectTimelineImageMobileProps) {
+    const [loaded, setIsLoaded] = useState(false);
+    const imageStyle = !loaded ? { display: "none" } : {}
+
     return (
-        <img src={props.url} alt={props.alt} className="project-img-mobile rounded-3 bg-green-1 p-1 m-1" />
+        <img
+            src={props.url}
+            alt={props.alt}
+            className="project-img-mobile rounded-3 bg-green-1 p-1 m-1"
+            onLoad={() => setIsLoaded(true)}
+            style={imageStyle}
+        />
     );
 }
 
