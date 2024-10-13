86cdcce4c38c1b02c8e1d2eb0473e25f200b56cd
Pawin Nakvisai <pawinnak2@gmail.com>
2024-10-13 20:37:32 +0700

    [UPDATE] Awards section

--- src/content/awards.ts
+++ src/content/awards.ts
@@ -1,13 +1,20 @@
 export const awards = [
     {
         id: 1,
+        name: 'SIT Academic Excellence Award (2024)',
+        date: 'August 2024',
+        description: 'Awarded a scholarship by my faculty for achieving excellent academic performance and engaging in academic volunteer activities.',
+        img: []
+    },
+    {
+        id: 2,
         name: 'SIT Academic Excellence Award (2023)',
         date: 'September 2023',
         description: 'Awarded a scholarship by my faculty for achieving excellent academic performance and engaging in academic volunteer activities.',
         img: []
     },
     {
-        id: 2,
+        id: 3,
         name: 'The Saed Lueang Rueng Rung Scholarship',
         date: 'July 2023',
         description: 'I am honored to be one of the 19 recipients of the Saed Lueang Rueng Rung Scholarship, an award from KMUTT granted to exceptionally skilled students in various fields, including academics, sports, arts and culture, innovation, or leadership. I was one of the three students who received this scholarship in the academic field.',
@@ -23,7 +30,7 @@ export const awards = [
         ]
     },
     {
-        id: 3,
+        id: 4,
         name: 'The Jia Ra Nai Petchra Scholarship Award in Academic Excellence (Honorary Certificate, 2021)',
         date: 'January 2023',
         description: 'Received an honorary certificate for achieving the highest cumulative grade and attaining the first rank among students in the same major.',
@@ -35,7 +42,7 @@ export const awards = [
         ]
     },
     {
-        id: 4,
+        id: 5,
         name: 'SIT Academic Excellence Award (2022)',
         date: 'September 2022',
         description: 'Awarded a scholarship by my faculty for achieving excellent academic performance and engaging in academic volunteer activities.',
